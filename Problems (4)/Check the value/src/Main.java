b964f6fb8e48214e5b0870bffe6582ffb667cf2d
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-04-11 22:05:05 +0530

    check weather input number is less than 10 or not

--- Problems (4)/Check the value/src/Main.java
+++ Problems (4)/Check the value/src/Main.java
@@ -4,5 +4,11 @@ class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         // put your code here
+        int n = scanner.nextInt();
+        if (n < 10) {
+            System.out.println("true");
+        } else {
+            System.out.println("false");
+        }
     }
 }
