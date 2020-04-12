bfa1a0e489f8620fc75962c984e0713bfad368bf
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-04-12 09:38:40 +0530

    creating greet() method

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -24,7 +24,7 @@ public class SimpleBot {
         //calling end() method
         end();
     }
-
+//creating greet() method
     static void greet(String assistantName, String birthYear) {
         System.out.println("Hello! My name is " + assistantName + ".");
         System.out.println("I was created in " + birthYear + ".");
