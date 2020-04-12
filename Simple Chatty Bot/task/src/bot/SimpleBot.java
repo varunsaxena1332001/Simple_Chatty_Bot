2cb5ce84f0d6bed75282df64e20ce9019f4088d0
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-04-12 10:33:25 +0530

    now scanning the variable @num

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -81,6 +81,7 @@ public class SimpleBot {
         System.out.println("Now I will prove to you that I can count to any number you want.");
 //creating variable @num
         //@num is of Integer type
+
         int num = scanner.nextInt();
         for (int i = 0; i <= num; i++) {
             System.out.printf("%d!\n", i);
