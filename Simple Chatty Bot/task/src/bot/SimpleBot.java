d2ca288c1d6261cad96307270a8b169c2c62dd3f
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-04-12 10:21:04 +0530

    creating variable @rem5

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -59,6 +59,7 @@ public class SimpleBot {
         //@rem3 is of Integer type
         //now scanning variable @rem3
         int rem3 = scanner.nextInt();
+        //creating variable @rem5
         int rem5 = scanner.nextInt();
         int rem7 = scanner.nextInt();
         int age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105;
