bf5a6836d62eaf62f54b44f21dfc9f0158ea4751
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-04-12 11:04:09 +0530

    creating main() method

--- Problems (3)/Replacing 'a' with 'b'/src/Main.java
+++ Problems (3)/Replacing 'a' with 'b'/src/Main.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 class Main {
+    /**
+     * creating main() method
+     */
     public static void main(String[] args) {
         // put your code here
         Scanner sc = new Scanner(System.in);
