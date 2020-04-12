973739dd894a124984440e8dfe830f894962b582
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-04-12 10:51:11 +0530

    end() method by default public type

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -129,6 +129,7 @@ public class SimpleBot {
  * creating end() method
  * end() method is not return type
  * end() method is static type
+ * end() method by default public type
  */
     static void end() {
         System.out.println("Congratulations, have a nice day!"); // Do not change this text
