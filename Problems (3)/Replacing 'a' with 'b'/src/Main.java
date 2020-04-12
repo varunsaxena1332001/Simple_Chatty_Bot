5e29f74128ed775bc0e6b212efe29622dfef4137
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-04-12 11:11:31 +0530

    for-loop is incrementation of 1 after every step

--- Problems (3)/Replacing 'a' with 'b'/src/Main.java
+++ Problems (3)/Replacing 'a' with 'b'/src/Main.java
@@ -15,7 +15,7 @@ class Main {
         String s = sc.next();
         //creating variable @s1 of String type and initialize it by ""
         String s1 = "";
-        //creating for-loop of the length of variable @s
+        //creating for-loop of the length of variable @s and incrementation of 1
         for (int i = 0; i < s.length(); i++) {
             if (s.charAt(i) == 'a') {
                 s1 = s1 + "b";
