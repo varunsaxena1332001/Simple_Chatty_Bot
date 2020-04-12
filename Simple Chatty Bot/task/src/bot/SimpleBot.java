89133fd424fa4653233b540b4cf11d9fdc057c65
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-04-12 10:29:43 +0530

    count() method is by default public type

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -75,6 +75,7 @@ public class SimpleBot {
 //creating count() method
 //count() method is not return type
 //count() method is of static type
+//count() method is by default public type
     static void count() {
         System.out.println("Now I will prove to you that I can count to any number you want.");
         int num = scanner.nextInt();
