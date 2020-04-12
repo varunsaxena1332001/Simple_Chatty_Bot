bb0f4efdd5e46a814f65349dc2fa7cf73309b834
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-04-12 12:49:54 +0530

    create main() method to print "WE NEED TO
                                   LEARN JAVA
    
                                   AS QUICKLY AS POSSIBLE"

---
+++ Problems (16)/We need to learn Java/src/Main.java
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+        // put your code here
+        System.out.println("WE NEED TO\n" +
+                "\n" +
+                "LEARN JAVA\n" +
+                "\n" +
+                "AS QUICKLY AS POSSIBLE");
+    }
+}
