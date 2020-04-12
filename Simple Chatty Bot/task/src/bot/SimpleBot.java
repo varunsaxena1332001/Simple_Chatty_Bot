e7160736fd6e5a53ca78bd6470ea1da3f52770a1
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2020-04-12 10:29:03 +0530

    count() method is of static type

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -74,6 +74,7 @@ public class SimpleBot {
     }
 //creating count() method
 //count() method is not return type
+//count() method is of static type
     static void count() {
         System.out.println("Now I will prove to you that I can count to any number you want.");
         int num = scanner.nextInt();
